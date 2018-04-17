0ce138e97ec9b19a8b6e3dae1883cdc80eaeee57
liuweixing <>
2018-04-17 21:01:35 +0800

    Foundation+Log.m 文件解析会有bug,采用宏 定义2种模式使用,挑选使用

--- 01AXTools/01AXCategory/Foundation+Log.m
+++ 01AXTools/01AXCategory/Foundation+Log.m
@@ -1,93 +1,100 @@
 
 #import <Foundation/Foundation.h>
 
-@implementation NSDictionary (Log)
-//- (NSString *)descriptionWithLocale:(id)locale
-//{
-//    NSMutableString *str = [NSMutableString string];
-//    
-//    [str appendString:@"{\n"];
-//    
-//    // 遍历字典的所有键值对
-//    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
-//        [str appendFormat:@"\t%@ = %@,\n", key, obj];
-//    }];
-//    
-//    [str appendString:@"}"];
-//    
-//    // 查出最后一个,的范围
-//    NSRange range = [str rangeOfString:@"," options:NSBackwardsSearch];
-//    if (range.length) {
-//        // 删掉最后一个,
-//        [str deleteCharactersInRange:range];
-//    }
-//    
-//    return str;
-//    
-////    NSString *output;
-////    @try {
-////        output = [[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding];
-////    } @catch (NSException *exception) {
-////        output = self.description;
-////    } @finally {
-////        
-////    }
-////    return  output;
-//}
+//#define USER_TYPE_ONE 1
 
+
+@implementation NSDictionary (Log)
+
+
+#if USER_TYPE_ONE
+
+- (NSString *)descriptionWithLocale:(id)locale{
+    NSMutableString *str = [NSMutableString string];
+    
+    [str appendString:@"{\n"];
+    
+    // 遍历字典的所有键值对
+    [self enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        [str appendFormat:@"\t%@ = %@,\n", key, obj];
+    }];
+    
+    [str appendString:@"}"];
+    
+    // 查出最后一个,的范围
+    NSRange range = [str rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.length) {
+        // 删掉最后一个,
+        [str deleteCharactersInRange:range];
+    }
+    
+    return str;
+}
+
+#else
 
 - (NSString *)descriptionWithLocale:(nullable id)locale{
-    NSString *logString;
+    NSString *output;
     @try {
-        logString=[[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding]; }
+        NSData *data = [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil];
+        output=[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        output = [output stringByReplacingOccurrencesOfString:@"\\/" withString:@"/"];
+    }
     @catch (NSException *exception) {
-        NSString *reason = [NSString stringWithFormat:@"reason:%@",exception.reason];
-        logString = [NSString stringWithFormat:@"转换失败:\n%@,\n转换终止,输出如下:\n%@",reason,self.description];
+        
+        output = self.description;
+        
     } @finally {
         
     }
-    return logString;
+    return output;
 }
 
+#endif
 
 @end
 
+
 @implementation NSArray (Log)
-//- (NSString *)descriptionWithLocale:(id)locale
-//{
-//    NSMutableString *str = [NSMutableString string];
-//    [str appendString:@"[\n"];
-//    
-//    // 遍历数组的所有元素
-//    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-//        [str appendFormat:@"%@,\n", obj];
-//    }];
-//    
-//    [str appendString:@"]"];
-//    // 查出最后一个,的范围
-//    NSRange range = [str rangeOfString:@"," options:NSBackwardsSearch];
-//    if (range.length) {
-//        // 删掉最后一个,
-//        [str deleteCharactersInRange:range];
-//    }
-//    
-//    return str;
-//    
-//    
-//}
+
+#if USER_TYPE_ONE
+
+- (NSString *)descriptionWithLocale:(id)locale{
+    NSMutableString *str = [NSMutableString string];
+    [str appendString:@"[\n"];
+    
+    // 遍历数组的所有元素
+    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+        [str appendFormat:@"%@,\n", obj];
+    }];
+    
+    [str appendString:@"]"];
+    // 查出最后一个,的范围
+    NSRange range = [str rangeOfString:@"," options:NSBackwardsSearch];
+    if (range.length) {
+        // 删掉最后一个,
+        [str deleteCharactersInRange:range];
+    }
+    return str;
+}
+
+#else
 
 - (NSString *)descriptionWithLocale:(nullable id)locale{
-    NSString *logString;
+    NSString *output;
     @try {
-        logString=[[NSString alloc] initWithData:[NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil] encoding:NSUTF8StringEncoding]; }
+        NSData *data = [NSJSONSerialization dataWithJSONObject:self options:NSJSONWritingPrettyPrinted error:nil];
+        output=[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        output = [output stringByReplacingOccurrencesOfString:@"\\/" withString:@"/"];
+    }
     @catch (NSException *exception) {
-        NSString *reason = [NSString stringWithFormat:@"reason:%@",exception.reason];
-        logString = [NSString stringWithFormat:@"转换失败:\n%@,\n转换终止,输出如下:\n%@",reason,self.description];
+        output = self.description;
     } @finally {
         
     }
-    return logString;
+    return output;
 }
 
+#endif
 
 @end
