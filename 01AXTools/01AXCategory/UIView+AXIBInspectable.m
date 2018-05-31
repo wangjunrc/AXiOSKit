d76c40870bd9a9be4fc6d4f9ae714b35df70b807
liuweixing <>
2018-05-31 23:43:09 +0800

    新增 UIView+AXIBInspectable,用于xib显示

---
+++ 01AXTools/01AXCategory/UIView+AXIBInspectable.m
@@ -0,0 +1,13 @@
+//
+//  UIView+AXIBInspectable.m
+//  AXiOSToolsDemo
+//
+//  Created by liuweixing on 2018/5/31.
+//  Copyright © 2018年 liuweixing. All rights reserved.
+//
+
+#import "UIView+AXIBInspectable.h"
+
+@implementation UIView (AXIBInspectable)
+
+@end
