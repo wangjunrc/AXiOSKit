a70b8bca5a805cc3fbdfa88211ceee0141103725
刘伟星.work <>
2018-12-04 20:46:25 +0800

    update

---
+++ AXiOSTools/01AXTools/05AXObject/AXConfigureFunction.h
@@ -0,0 +1,17 @@
+//
+//  AXConfigureFunction.h
+//  AXTools
+//
+//  Created by AXing on 2018/12/4.
+//  Copyright © 2018 liuweixing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AXConfigureFunction : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AXiOSTools/01AXTools/05AXObject/AXConfigureFunction.m
@@ -0,0 +1,21 @@
+//
+//  AXConfigureFunction.m
+//  AXTools
+//
+//  Created by AXing on 2018/12/4.
+//  Copyright © 2018 liuweixing. All rights reserved.
+//
+
+#import "AXConfigureFunction.h"
+
+@implementation AXConfigureFunction
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
