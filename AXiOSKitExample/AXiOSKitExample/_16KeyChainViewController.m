b4c7e1d4635d377a14774bc47520aa2c405ee063
axinger <liu_weixing@qq.com>
2020-08-06 17:15:23 +0800

    update…

--- AXiOSKitExample/AXiOSKitExample/_16KeyChainViewController.m
+++ AXiOSKitExample/AXiOSKitExample/_16KeyChainViewController.m
@@ -13,6 +13,8 @@
 
 @interface _16KeyChainViewController ()
 
+@property (strong, nonatomic) NSDictionary *query;
+
 @end
 
 @implementation _16KeyChainViewController
@@ -50,5 +52,105 @@
         AXLoger(@"result = %d", result);
     }];
 }
+
+static NSString *const KEYCHAIN_SERVICE = @"flutter_keychain";
+
+
+- (instancetype)init {
+    self = [super init];
+    if (self){
+        self.query = @{
+            (__bridge id)kSecClass :(__bridge id)kSecClassGenericPassword,
+            (__bridge id)kSecAttrService :KEYCHAIN_SERVICE,
+        };
+    }
+    return self;
+}
+
+static NSString *const getDeviceIDKey = @"getDeviceIDKey";
+
+
+-(NSString *)getDeviceID{
+    
+  NSString *value = [self get:getDeviceIDKey];
+    if (value.length==0) {
+        value = [self ax_uuid];
+        [self set:value forKey:getDeviceIDKey];
+    }
+    
+    return value;
+}
+
+/// 生成uuid
+-(NSString *)ax_uuid{
+    CFUUIDRef uuidRef = CFUUIDCreate(NULL);
+    CFStringRef uuidStrRef= CFUUIDCreateString(NULL, uuidRef);
+    CFRelease(uuidRef);
+    NSString *uuidStr = (__bridge NSString *)uuidStrRef;
+    CFRelease(uuidStrRef);
+    return uuidStr;
+}
+
+
+
+///============================通用key========================================
+- (NSString *)get:(NSString *)key {
+    NSMutableDictionary *search = [self.query mutableCopy];
+    search[(__bridge id)kSecAttrAccount] = key;
+    search[(__bridge id)kSecReturnData] = (__bridge id)kCFBooleanTrue;
+
+    CFDataRef resultData = NULL;
+
+    OSStatus status;
+    status = SecItemCopyMatching((__bridge CFDictionaryRef)search, (CFTypeRef*)&resultData);
+    NSString *value;
+    if (status == noErr){
+        NSData *data = (__bridge NSData*)resultData;
+        value = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    }
+    return value;
+}
+
+
+- (void)set:(NSString *)value forKey:(NSString *)key {
+    NSMutableDictionary *search = [self.query mutableCopy];
+    search[(__bridge id)kSecAttrAccount] = key;
+    search[(__bridge id)kSecMatchLimit] = (__bridge id)kSecMatchLimitOne;
+
+    OSStatus status;
+    status = SecItemCopyMatching((__bridge CFDictionaryRef)search, NULL);
+    if (status == noErr){
+        search[(__bridge id)kSecMatchLimit] = nil;
+
+        NSDictionary *update = @{(__bridge id)kSecValueData: [value dataUsingEncoding:NSUTF8StringEncoding]};
+
+        status = SecItemUpdate((__bridge CFDictionaryRef)search, (__bridge CFDictionaryRef)update);
+        if (status != noErr){
+            NSLog(@"SecItemUpdate status = %d", status);
+        }
+    }else{
+        search[(__bridge id)kSecValueData] = [value dataUsingEncoding:NSUTF8StringEncoding];
+        search[(__bridge id)kSecMatchLimit] = nil;
+
+        status = SecItemAdd((__bridge CFDictionaryRef)search, NULL);
+        if (status != noErr){
+            NSLog(@"SecItemAdd status = %d", status);
+        }
+    }
+}
+
+- (void)remove:(NSString *)key {
+    NSMutableDictionary *search = [self.query mutableCopy];
+    search[(__bridge id)kSecAttrAccount] = key;
+    search[(__bridge id)kSecReturnData] = (__bridge id)kCFBooleanTrue;
+    SecItemDelete((__bridge CFDictionaryRef)search);
+}
+
+- (void)clear {
+    NSMutableDictionary *search = [self.query mutableCopy];
+    SecItemDelete((__bridge CFDictionaryRef)search);
+}
+
+
 
 @end
