3b3302160624f200ce0ec117b9d1f34059925f63
axinger <>
2020-09-02 19:56:19 +0800

    update…

--- AXiOSKitExample/AXiOSKitExample/H5.bundle/js/index.js
+++ AXiOSKitExample/AXiOSKitExample/H5.bundle/js/index.js
@@ -1,24 +1,23 @@
 document.write("<script language=javascript src='js/client.js'></script>");
 
 
-
 var button1 = document.getElementById('button1');
-button1.addEventListener('click',function () {
-                         
-                         buttonClick1();
-                         });
+button1.addEventListener('click', function () {
 
-document.getElementById('button2').addEventListener('click',function () {
-                                                    buttonClick2();
-                                                    });
+    buttonClick1();
+});
 
+document.getElementById('button2').addEventListener('click', function () {
+    buttonClick2();
+});
 
 
 function showAler(data) {
     alert(data);
-    return data+'收到'
+    return data + '收到'
     //    iosWeixinPay();
 }
+
 function showAler2() {
     alert(4);
     //    iosWeixinPay();
@@ -26,32 +25,31 @@ function showAler2() {
 }
 
 
-
 // js调oc 原生注入方式
 function buttonClick1() {
-    
+
     var person = new Person();
     person.name = 'jim';
     person.age = 12;
     person.isAll = true;
     if (isiOS) {
-        
+
         window.webkit.messageHandlers.person.postMessage(person);
-    }else{
-        
-        
+    } else {
+
+
     }
-    
+
 }
 
 function buttonClick2() {
-    
-    if(isiOS){
-      alert("ios平台");
-    }else{
+
+    if (isiOS) {
+        alert("ios平台");
+    } else {
         alert("其他平台");
     }
-    
+
 }
 
 function buttonClick() {
@@ -59,7 +57,7 @@ function buttonClick() {
     //ocTestName oc 需要js注入的名称
     alert("A");
     window.webkit.messageHandlers.ocTestName.postMessage('jim');
-    
+
 }
 
 //全局变量
@@ -67,7 +65,7 @@ var globalPerson = new Person() //用一个构造函数来创建了对象
 
 
 // 定义对象
-function Person(name, age,isAll) { //创建一个person的函数
+function Person(name, age, isAll) { //创建一个person的函数
     this.name = name; //此处的this对应的是对象obj
     this.age = age
     this.isAll = isAll
@@ -75,18 +73,17 @@ function Person(name, age,isAll) { //创建一个person的函数
 
 
 function payCallBack(data) {
-    
+
     alert(data);
-    
+
 }
 
 function iosWeixinPay() {
-    
+
     globalPerson.name = 'tom'
     globalPerson.age = 12
-    
-    
-    
+
+
 }
 
 
