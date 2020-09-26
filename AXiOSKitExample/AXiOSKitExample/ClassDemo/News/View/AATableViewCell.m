bfce1c0c9f685c939ecb822ab10264efa0a756aa
axinger <axingrun@outlook.com>
2020-09-26 12:15:52 +0800

    update…

---
+++ AXiOSKitExample/AXiOSKitExample/ClassDemo/News/View/AATableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  AATableViewCell.m
+//  AXiOSKitExample
+//
+//  Created by 小星星吃KFC on 2020/9/26.
+//  Copyright © 2020 liuweixing. All rights reserved.
+//
+
+#import "AATableViewCell.h"
+
+@implementation AATableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
