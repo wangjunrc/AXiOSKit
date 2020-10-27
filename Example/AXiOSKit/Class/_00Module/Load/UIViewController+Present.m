44e057e5d2662a00761ed134477cf3d32184fc33
axinger <axingrun@outlook.com>
2020-10-27 14:53:53 +0800

    update…

--- Example/AXiOSKit/Class/_00Module/Load/UIViewController+Present.m
+++ Example/AXiOSKit/Class/_00Module/Load/UIViewController+Present.m
@@ -12,13 +12,13 @@
 @implementation UIViewController (Present)
 
 + (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Method presentM = class_getInstanceMethod(self.class, @selector(presentViewController:animated:completion:));
-        Method presentSwizzlingM = class_getInstanceMethod(self.class, @selector(dy_presentViewController:animated:completion:));
-        // 交换方法实现
-        method_exchangeImplementations(presentM, presentSwizzlingM);
-    });
+//    static dispatch_once_t onceToken;
+//    dispatch_once(&onceToken, ^{
+//        Method presentM = class_getInstanceMethod(self.class, @selector(presentViewController:animated:completion:));
+//        Method presentSwizzlingM = class_getInstanceMethod(self.class, @selector(dy_presentViewController:animated:completion:));
+//        // 交换方法实现
+//        method_exchangeImplementations(presentM, presentSwizzlingM);
+//    });
     
     /// usingBlock: 第一个参数 调用对象,第二个是方法的第一次参数
      [UIViewController aspect_hookSelector:@selector(presentViewController:animated:completion:) withOptions:AspectPositionInstead usingBlock:^(id<AspectInfo> aspectInfo,UIViewController *presentViewController) {
