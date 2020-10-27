0420737142cc482920aad6a80637066b77ed38d2
axinger <axingrun@outlook.com>
2020-10-27 14:48:41 +0800

    update…

--- Example/AXiOSKit/Class/_00Module/Load/UIImage+Load.m
+++ Example/AXiOSKit/Class/_00Module/Load/UIImage+Load.m
@@ -18,18 +18,30 @@
 
 + (void)load {
     
-//    static dispatch_once_t onceToken;
-//    dispatch_once(&onceToken, ^{
+    //    id instance = info.instance;               //调用的实例对象
+    //    id invocation = info.originalInvocation;   //原始的方法
+    //    id arguments = info.arguments;             //参数
+    //    [invocation invoke];                       //原始的方法，再次调用
+    
 //        NSError *error;
-//        [object_getClass(UIImage.class) aspect_hookSelector:@selector(imageNamed:) withOptions:AspectPositionBefore usingBlock:^(id<AspectInfo> aspectInfo) {
-//            id obj = aspectInfo.instance;
-//            NSLog(@"ax_imageNamed == %@",obj);
+//        [object_getClass(UIImage.class) aspect_hookSelector:@selector(imageNamed:) withOptions:AspectPositionAfter usingBlock:^(id<AspectInfo> aspectInfo,NSString *imageNamed) {
+//            NSInvocation  *invocation = aspectInfo.originalInvocation;
+//            if ([aspectInfo.arguments.firstObject isEqualToString:@"ax_icon_weixin"]) {
+//                id img= nil;
+//                img= [UIImage imageNamed:@"chongshe"];
+//                [invocation setReturnValue:&img];
+//                NSLog(@"img %@",img);
+//            }
 //        } error:&error];
 //        NSLog(@"ax_imageNamed error== %@",error);
-//    });
+    
+//    [UIImage ax_replaceClassMethodWithOriginal:@selector(imageNamed:) newSelector:@selector(ax_imageNamed:)];
 }
+
 + (nullable UIImage *)ax_imageNamed:(NSString *)name{
-    NSLog(@"ax_imageNamed == %@",name);
+    if ([name isEqualToString:@"ax_icon_weixin"]) {
+        name =@"chongshe" ;
+    }
     return [self ax_imageNamed:name];
 }
 
