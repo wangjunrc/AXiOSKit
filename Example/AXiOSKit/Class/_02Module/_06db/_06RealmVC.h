380ba1f8673714d39b74c80a3d092ed446d7c51b
axinger <axingrun@outlook.com>
2021-07-25 20:11:18 +0800

    update…

---
+++ Example/AXiOSKit/Class/_02Module/_06db/_06RealmVC.h
@@ -0,0 +1,17 @@
+//
+//  _06RealmVC.h
+//  AXiOSKit
+//
+//  Created by 小星星吃KFC on 2021/7/24.
+//  Copyright © 2021 axinger. All rights reserved.
+//
+
+#import "AXDemoBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface _06RealmVC : AXDemoBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/AXiOSKit/Class/_02Module/_06db/_06RealmVC.m
@@ -0,0 +1,32 @@
+//
+//  _06RealmVC.m
+//  AXiOSKit
+//
+//  Created by 小星星吃KFC on 2021/7/24.
+//  Copyright © 2021 axinger. All rights reserved.
+//
+
+#import "_06RealmVC.h"
+
+@interface _06RealmVC ()
+
+@end
+
+@implementation _06RealmVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
