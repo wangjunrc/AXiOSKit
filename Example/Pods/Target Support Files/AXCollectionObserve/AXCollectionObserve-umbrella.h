8104bfd2a3791c8015fa35ae9eb733315c3fc92e
axinger <axinger@outlook.com>
2021-07-27 08:57:35 +0800

    update…

---
+++ Example/Pods/Target Support Files/AXCollectionObserve/AXCollectionObserve-umbrella.h
@@ -0,0 +1,24 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AXCollectionObserve.h"
+#import "RACSignal+AXMutable.h"
+#import "AXMutableHelper.h"
+#import "AXMutableObserveProtocol.h"
+#import "NSMutableArray+AXObserve.h"
+#import "NSMutableDictionary+AXObserve.h"
+#import "NSMutableOrderedSet+AXObserve.h"
+#import "NSMutableSet+AXObserve.h"
+
+FOUNDATION_EXPORT double AXCollectionObserveVersionNumber;
+FOUNDATION_EXPORT const unsigned char AXCollectionObserveVersionString[];
+
