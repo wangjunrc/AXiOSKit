fcbacccbce20013d358f58e54f5c7727dc13ef6d
axinger <axinger@outlook.com>
2021-09-16 16:38:47 +0800

    update…

--- Example/AXiOSKit/Class/_00Material/_TestSwift/DogSwift.swift
+++ Example/AXiOSKit/Class/_00Material/_TestSwift/DogSwift.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-
+import UIKit
 @objcMembers class DogSwift: NSObject {
     func show() {
         print("DogSwift");
@@ -15,11 +15,18 @@ import Foundation
     func showOC() {
         let dog :DogOC  = DogOC.init();
         dog.show();
+        /// Literal 提示
+        /// Image Literal  提示图片
+    
         
+        /// https://www.hangge.com/blog/cache/detail_1903.html
+        let imgView : UIImageView = UIImageView()
+        imgView.frame = CGRect.init(x: 0, y: 0, width: 300, height: 300 )
+        imgView.image = #imageLiteral(resourceName: "西瓜")
     }
     
     
-//    func reg() -> Void {
-//        MonkeyKing .registerAccount(.weChat(appID: <#T##String#>, appKey: <#T##String?#>, miniAppID: <#T##String?#>, universalLink: <#T##String?#>));
-//    }
+    //    func reg() -> Void {
+    //        MonkeyKing .registerAccount(.weChat(appID: <#T##String#>, appKey: <#T##String?#>, miniAppID: <#T##String?#>, universalLink: <#T##String?#>));
+    //    }
 }
